e17f2e90417c8ac5dbe072f8e275456ec6260eb0
GiuseppeCutrano <giuseppecutrano.info@gmail.com>
2020-12-17 18:18:48 +0100

    nav e complete

--- dist/app.js
+++ dist/app.js
@@ -97,10 +97,10 @@
 
 /***/ }),
 
-/***/ "./src/app.scss":
-/*!**********************!*\
-  !*** ./src/app.scss ***!
-  \**********************/
+/***/ "./src/style/app.scss":
+/*!****************************!*\
+  !*** ./src/style/app.scss ***!
+  \****************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -109,14 +109,14 @@
 /***/ }),
 
 /***/ 0:
-/*!*****************************************!*\
-  !*** multi ./src/app.js ./src/app.scss ***!
-  \*****************************************/
+/*!***********************************************!*\
+  !*** multi ./src/app.js ./src/style/app.scss ***!
+  \***********************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! C:\Users\Giuseppe\Desktop\node-sass-teambit\src\app.js */"./src/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Giuseppe\Desktop\node-sass-teambit\src\app.scss */"./src/app.scss");
+module.exports = __webpack_require__(/*! C:\Users\Giuseppe\Desktop\node-sass-teambit\src\style\app.scss */"./src/style/app.scss");
 
 
 /***/ })
